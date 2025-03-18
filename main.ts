6f41cf0935cb957b315b06e1f5d04b8c3362460b
KaduMasiero <masierokadu@gmail.com>
2025-03-18 15:44:59 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,38 @@
+basic.forever(function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . . . # .
+        . . . # .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # # # . .
+        . # . . .
+        . # # . .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . # # . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . . . # .
+        . . # # .
+        . # # . .
+        `)
+})
